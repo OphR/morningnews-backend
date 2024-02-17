1a92c9ec5868339e3f09734e81bf5a164dee917d
Ophélie <roux.ophelie.or@gmail.com>
2024-02-17 10:36:32 +0100

    mm

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const fetch = require('node-fetch');
 const NEWS_API_KEY = process.env.NEWS_API_KEY;
 
 router.get('/articles', (req, res) => {
-  fetch(`http://newsapi.org/v2/top-headlines?sources=tech-crunch&apiKey=c7cf4caa6117469a9ce126d1a741910c`)
+  fetch(`https://newsapi.org/v2/top-headlines?sources=tech-crunch&apiKey=c7cf4caa6117469a9ce126d1a741910c`)
     .then(response => response.json())
     .then(data => {
       if (data.status === 'ok') {
